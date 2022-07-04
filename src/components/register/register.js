eea3b479d87c9da809d96715db69641f88ea099d
Vishal Pandey <vishalpandey10022000@gmail.com>
2022-07-05 02:34:44 +0530

    added api link

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -25,7 +25,7 @@ const Register = () => {
     const register = () => {
         const { name, email, password, reEnterPassword } = user
         if( name && email && password && (password === reEnterPassword)){
-            axios.post("http://localhost:9000/register", user)
+            axios.post("https://login-register-mern-app12.herokuapp.com//register", user)
             .then( res => {
                 alert(res.data.message)
                 history.push("/login")
